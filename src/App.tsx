a9edd4e3a58680a6344bdf3d4ff073b69cfa7f35
Богдан <sysoevbogdan82@gmail.com>
2024-12-02 16:06:28 +0200

    set up the project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nam ea
+      omnis modi quo perferendis fugiat ipsam odio minima hic?
+    </div>
+  );
+};
+
+export default App;
---
+++ src/pages/tariffs-page/TariffsPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TariffsPage = () => {
+  return <div>TariffsPage</div>;
+};
+
+export default TariffsPage;
