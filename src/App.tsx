ff8d54f1cf4845193e612b9ad95e619a10267004
Богдан <sysoevbogdan82@gmail.com>
2024-12-02 16:16:05 +0200

    added class for another font family

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 const App = () => {
   return (
-    <div>
+    <div className="istok-web">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nam ea
       omnis modi quo perferendis fugiat ipsam odio minima hic?
     </div>
