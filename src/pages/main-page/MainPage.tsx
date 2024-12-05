fd2749b07bde45167bac3a4a3fcae902d1506090
eralinkd <eralinkd@gmail.com>
2024-12-05 18:06:05 +0200

    main page

--- src/pages/main-page/MainPage.tsx
+++ src/pages/main-page/MainPage.tsx
@@ -1,5 +1,53 @@
+import { useEffect, useState } from 'react';
+
+import Button from '../../shared/button/Button';
+import clsx from 'clsx';
+import styles from './MainPage.module.scss';
+
 const MainPage = () => {
-  return <div>MainPage</div>;
+  const [keyV, setKeyV] = useState<string>('ss://hD12SN123JNSHBjDHb2V2gn12n323as');
+  const [isCopied, setIsCopied] = useState<boolean>(false);
+
+  useEffect(() => {
+    setKeyV('ss://hD12SN123JNSHBjDHb2V2gn12n323as');
+  }, []);
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(keyV);
+    setIsCopied(true);
+
+    setTimeout(() => setIsCopied(false), 700);
+  };
+
+  return (
+    <section className={styles.section}>
+      <h2>Осталось</h2>
+      <div className={styles.circle}>
+        377 GB
+      </div>
+      <p className={styles.upd}>Обновляется ежемесячно</p>
+
+      <div className={styles.keyContainer}>
+        <label htmlFor="key">Ваш ключ для Outline</label>
+        <div>
+          <input onClick={handleCopy} name='key' readOnly type="text" value={keyV}></input>
+          <button onClick={handleCopy}></button>
+        </div>
+        <p onClick={handleCopy}>Нажмите, чтобы скопировать</p>
+      </div>
+
+      <div className={styles.info}>
+          <p>Подписка: <span className={clsx(styles.subscription, styles.premium)}>Премиум</span></p>
+          <p>Действует до: <span className={styles.date}>27/12/2025</span></p>
+      </div>
+
+      <Button text="Продлить"></Button>
+
+      <div className={clsx(styles.copied, isCopied && styles.active)}>
+        Ключ скопирован!
+      </div>
+    </section>
+  );
 };
 
 export default MainPage;
--- src/shared/button/Button.tsx
+++ src/shared/button/Button.tsx
@@ -1,5 +1,18 @@
-const Button = () => {
-  return <div>Butto</div>;
+import clsx from 'clsx';
+import styles from './Button.module.scss';
+
+interface ButtonProps {
+  text: string;
+  className?: string;
+  onClick?: () => void;
+}
+
+const Button = ({ text, className, onClick }: ButtonProps) => {
+  return (
+    <button onClick={onClick} className={clsx(styles.button, className)}>
+      {text}
+    </button>
+  );
 };
 
 export default Button;
